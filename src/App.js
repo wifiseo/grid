03899fd2fce94194845ef7f66b3540b52868339d
wifiseo <wifiseoservice@gmail.com>
2022-08-26 11:56:41 +0600

    adding more grind

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   const details = [
     {title: 'Car BMW 2017 Model', price: 10},
     {title: 'Mdern CD3 2049 electric', price: 20},
-    {title: '2 whele model pandamic', price: 30}
+    {title: '2 whele model pandamic', price: 30},
+    {title: 'Mdern CD3 2049 electric', price: 20},
+    {title: 'Mdern CD3 2049 electric', price: 20},
+    {title: 'Mdern CD3 2049 electric', price: 20},
+    {title: 'Mdern CD3 2049 electric', price: 20},
+    {title: 'Mdern CD3 2049 electric', price: 20},
     ];
 
   const elements = details.map(x => <Grid title={x.title}></Grid>);
@@ -24,9 +29,11 @@ function Grid(props){
       <div style={{color: '#fff', backgroundColor:'#0DC167', display:'block', width: '129px', textAlign:'center', position: 'relative', left:'180px', padding:'7px', transform: 'rotate(45deg' }}>Booked</div>
       <div style={{backgroundColor:'#1E3560', height:'50px', width:'20px', position:'relative',top:'-3px', left:'231px'}}></div>
       <div style={{backgroundColor:'#1E3560', height:'30px', width:'50px', position:'relative',top:'-135px', left:'140px'}}></div>
-      <img style={{maxWidth:'50%'}} src={logo}/>
-      <h2 style={{color: '#fff'}}>{props.title}</h2>
-      <button style={{border:'none', cursor: 'pointer', backgroundColor: '#0DC167',fontSize:'18px', color:'#fff', padding:'15px'}}>Booking Now</button>
+      <div style={{display:'flex', flexDirection:'column', alignItems:'center', position:'relative', top:'-50px'}}>
+        <img style={{maxWidth:'50%'}} src={logo}/>
+        <h2 style={{color: '#fff'}}>{props.title}</h2>
+        <button style={{border:'none', cursor: 'pointer', backgroundColor: '#0DC167',fontSize:'18px', color:'#fff', padding:'15px'}}>Booking Now</button>
+      </div>
       </div>
     </div>
   )
